e214ec128e32ec6d2788862e22a9d298349b13d8
Stephen Trewick <stephent818@gmail.com>
2019-12-04 13:56:47 -0800

    routing containers to App

---
+++ src/containers/Forum/Forum.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Forum extends Component {
+
+  render() {
+    return (
+      <div>
+        This is the Forum page.
+      </div>
+    );
+  }
+}
+
+export default Forum;
