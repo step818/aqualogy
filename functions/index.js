447de28aa781358b8f25441f7a627df838ae4a84
Stephen Trewick <stephent818@gmail.com>
2019-12-20 13:52:16 -0800

    newUser notiffication function deployed and working

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,14 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello, magical people!");
-});
-
 const createNotification = ((notification) => {
   return admin.firestore().collection('notifications')
     .add(notification)
     .then(doc => console.log('notification added', doc))
+});
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello, magical people!");
 });
 
 exports.blogCreated = functions.firestore
@@ -25,3 +25,20 @@ exports.blogCreated = functions.firestore
 
     return createNotification(notification);
 });
+
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'New user joined',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        }
+
+        return createNotification(notification);
+      });
+  });
