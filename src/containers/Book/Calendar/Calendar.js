d605442bc5d82572fd400c6262c7f44d1867c76b
Stephen Trewick <stephent818@gmail.com>
2019-12-30 18:50:43 -0800

    added to calendar component, mapping out days of week

--- src/containers/Book/Calendar/Calendar.js
+++ src/containers/Book/Calendar/Calendar.js
@@ -10,7 +10,7 @@ class Calendar extends Component {
   }
 
   state = {
-    momentContext: moment(),
+    dateContext: moment(),
     today: moment(),
     showMonthPopup: false,
     showYearPopup: false
@@ -20,11 +20,48 @@ class Calendar extends Component {
   weekdaysShort = moment.weekdaysShort();  // ["Sun", "Mon", "Tue", ... ]
   months = moment.months();
 
+  year = () => {
+    return this.state.dateContext.format("Y");
+  }
+  month = () => {
+    return this.state.dateContext.format("MMMM");
+  }
+  daysInMonth = () => {
+    return this.state.dateContext.daysInMonth();
+  }
+  currentDate = () => {
+    return this.state.dateContext.get("date");
+  }
+  currentDay = () => {
+    return this.state.dateContext.format("D");
+  }
+  firstDayOfMonth = () => {
+    let dateContext = this.state.dateContext;
+    let firstDay = moment(dateContext).startOf('month').format('d');  // Day of week 0...1...5...6
+    return firstDay;
+  }
+
   
   render() {
+    // Map the weekdays, i.e. Sun, Mon, Tue, Wed, ... as <td>
+    let weekdays = this.weekdaysShort.map((day) => {
+      return (
+        <td key={day} className={classes.weekDay}>{day}</td>
+      )
+    });
+    
     return(
       <div className={classes.calendarContainer}>
-        <h2>Calendar</h2>
+        <table className={classes.calendar}>
+          <thead>
+            <tr className={classes.calendarHeader}>
+
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
       </div>
     );
   }
